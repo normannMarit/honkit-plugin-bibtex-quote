459e1d7ce5f5d0a5fd49d09387a560c505a3f844
normannmarit <normann.marit@gmail.com>
2022-04-06 14:06:30 +0200

    [test]: Minimal layout update

--- index.js
+++ index.js
@@ -1,127 +1,151 @@
-var bibtexParse = require('bibtex-parser-js');
-var _ = require('lodash');
-var fs = require('fs');
+var bibtexParse = require("bibtex-parser-js");
+var _ = require("lodash");
+var fs = require("fs");
 
 module.exports = {
-    book: {
-        assets: './assets',
-        css: [
-            "style.css"
-        ]
-    },
+  book: {
+    assets: "./assets",
+    css: ["style.css"],
+  },
 
-    filters: {
-        cite: function(key, additionalText) {
-            var citation = _.find(this.config.get('bib'), {'citationKey': key.toUpperCase()});
-            if (citation !== undefined) {
-                if (!citation.used) {
-                    citation.used = true;
-                    this.config.set('bibCount', this.config.get('bibCount') + 1);
-                    citation.number = this.config.get('bibCount');
-                }
-                var citationText = citation.number;
-                if (additionalText) {
-                    additionalText = additionalText.replace(/~/g, "&nbsp;");
-                    citationText += ", " + additionalText;
-                }
-                return '<a href="#cite-' + citation.number + '">[' + citationText + ']</a>';
-            } else {
-                return "[Citation not found]";
-            }
+  filters: {
+    cite: function (key, additionalText) {
+      var citation = _.find(this.config.get("bib"), {
+        citationKey: key.toUpperCase(),
+      });
+      if (citation !== undefined) {
+        if (!citation.used) {
+          citation.used = true;
+          this.config.set("bibCount", this.config.get("bibCount") + 1);
+          citation.number = this.config.get("bibCount");
         }
-    },
-
-    hooks: {
-        init: function() {
-            var bib = fs.readFileSync('literature.bib', 'utf8');
-            this.config.set('bib', bibtexParse.toJSON(bib));
-            this.config.set('bibCount', 0);
+        var citationText = citation.number;
+        if (additionalText) {
+          additionalText = additionalText.replace(/~/g, "&nbsp;");
+          citationText += ", " + additionalText;
         }
+        return (
+          '<a href="#cite-' + citation.number + '">[' + citationText + "]</a>"
+        );
+      } else {
+        return "[Citation not found]";
+      }
     },
+  },
 
-    blocks: {
-        references: {
-            process: function(blk) {
-                var usedBib = _.filter(this.config.get('bib'), 'used');
-                var sortedBib = _.sortBy(usedBib, 'number');
+  hooks: {
+    init: function () {
+      var bib = fs.readFileSync("literature.bib", "utf8");
+      this.config.set("bib", bibtexParse.toJSON(bib));
+      this.config.set("bibCount", 0);
+    },
+  },
 
-                var result = '<table class="references">';
+  blocks: {
+    references: {
+      process: function (_blk) {
+        var usedBib = _.filter(this.config.get("bib"), "used");
+        var sortedBib = _.sortBy(usedBib, "number");
 
-                sortedBib.forEach(function(item) {
-                    result += '<tr><td><span class="citation-number" id="cite-' + item.number + '">' + item.number + '</span></td><td>';
-                    var defaultKeys = ['AUTHOR', 'TITLE', 'BOOKTITLE', 'PUBLISHER', 'YEAR'];
-                    var keysForTypes = {
-                        'ONLINE': {'keys': ['AUTHOR', 'PLAIN_TITLE', 'SUBTITLE', 'NOTE', 'URL'], 'separator': '. '},
-                    };
+        var result = "<div>";
 
-                    var keys = defaultKeys;
-                    var separator = ', ';
-                    if (item.entryType in keysForTypes) {
-                        var obj = keysForTypes[item.entryType];
-                        keys = obj.keys;
-                        separator = obj.separator;
-                    }
-                    var values = valuesForKeys(keys, getTagsDictionary(item.entryTags));
-                    if (values.length > 0) {
-                        result += values.join(separator) + ".";
-                    }
+        sortedBib.forEach(function (item) {
+          result +=
+            '<blockquote id="cite-' +
+            item.number +
+            '"><sup>' +
+            item.number +
+            "</sup>. ";
+          var defaultKeys = [
+            "AUTHOR",
+            "TITLE",
+            "BOOKTITLE",
+            "PUBLISHER",
+            "YEAR",
+            "NOTE",
+          ];
+          var keysForTypes = {
+            ONLINE: {
+              keys: ["AUTHOR", "PLAIN_TITLE", "SUBTITLE", "NOTE", "URL"],
+              separator: ". ",
+            },
+          };
 
-                    result += '</td></tr>';
-                });
+          var keys = defaultKeys;
+          var separator = ", ";
+          if (item.entryType in keysForTypes) {
+            var obj = keysForTypes[item.entryType];
+            keys = obj.keys;
+            separator = obj.separator;
+          }
+          var values = valuesForKeys(keys, getTagsDictionary(item.entryTags));
+          if (values.length > 0) {
+            result += values.join(separator) + ".";
+          }
 
-                result += '</table>';
+          result += "</blockquote>";
+        });
 
-                return result;
-            }
-        }
-    }
+        result += "</div>";
+
+        return result;
+      },
+    },
+  },
 };
 
 function valuesForKeys(keys, dict) {
-    var values = [];
-    keys.forEach(function(key) {
-        if (key in dict) {
-            values.push(dict[key]);
-        }
-    });
-    return values;
+  var values = [];
+  keys.forEach(function (key) {
+    if (key in dict) {
+      values.push(dict[key]);
+    }
+  });
+  return values;
 }
 
 function getTagsDictionary(entryTags) {
-    var tags = entryTags;
-    if (entryTags.AUTHOR) {
-        tags['AUTHOR'] = formatAuthors(entryTags.AUTHOR);
+  var tags = entryTags;
+  if (entryTags.AUTHOR) {
+    tags["AUTHOR"] = formatAuthors(entryTags.AUTHOR);
+  }
+  if (entryTags.TITLE) {
+    tags["TITLE_PLAIN"] = entryTags.TITLE;
+    if (entryTags.URL) {
+      tags["URL"] = '<a href="' + entryTags.URL + '">' + entryTags.URL + "</a>";
+      tags["TITLE"] =
+        '<a href="' + entryTags.URL + '">' + entryTags.TITLE + "</a>";
+    } else {
+      tags["TITLE"] = entryTags.TITLE;
     }
-    if (entryTags.TITLE) {
-        tags['TITLE_PLAIN'] = entryTags.TITLE;
-        if (entryTags.URL) {
-            tags['URL'] = '<a href="' + entryTags.URL + '">' + entryTags.URL + '</a>';
-            tags['TITLE'] = '<a href="' + entryTags.URL + '">' + entryTags.TITLE + '</a>';
-        } else {
-            tags['TITLE'] = entryTags.TITLE;
-        }
+  }
+  if (entryTags.BOOKTITLE) {
+    tags["BOOKTITLE_PLAIN"] = entryTags.BOOKTITLE;
+    if (entryTags.BOOKURL) {
+      tags["BOOKURL"] =
+        '<a href="' + entryTags.BOOKURL + '">' + entryTags.BOOKURL + "</a>";
+      tags["BOOKTITLE"] =
+        '<a href="' + entryTags.BOOKURL + '">' + entryTags.BOOKTITLE + "</a>";
+    } else {
+      tags["BOOKTITLE"] = "<i>" + entryTags.BOOKTITLE + "</i>";
     }
-    if (entryTags.BOOKTITLE) {
-        tags['BOOKTITLE_PLAIN'] = entryTags.BOOKTITLE;
-        if (entryTags.BOOKURL) {
-            tags['BOOKURL'] = '<a href="' + entryTags.BOOKURL + '">' + entryTags.BOOKURL + '</a>';
-            tags['BOOKTITLE'] = '<a href="' + entryTags.BOOKURL + '">' + entryTags.BOOKTITLE + '</a>';
-        } else {
-            tags['BOOKTITLE'] = '<i>' + entryTags.BOOKTITLE + '</i>';
-        }
-    }
-    if (entryTags.PUBLISHER) {
-        tags['PUBLISHER'] = '<i>' + entryTags.PUBLISHER + '</i>';
-    }
-    return tags;
+  }
+  if (entryTags.PUBLISHER) {
+    tags["PUBLISHER"] = "<i>" + entryTags.PUBLISHER + "</i>";
+  }
+
+  if (entryTags.NOTE) {
+    tags["NOTE"] = "<br /><i>" + entryTags.NOTE + "</i>";
+  }
+  return tags;
 }
 
-function formatAuthors (authorsString) {
-    var authors = authorsString.split('and');
+function formatAuthors(authorsString) {
+  var authors = authorsString.split("and");
 
-    if (authors.length > 3) {
-        return authors[0] + ' <i>et al.</i>';
-    } else {
-        return authorsString;
-    }
+  if (authors.length > 3) {
+    return authors[0] + " <i>et al.</i>";
+  } else {
+    return authorsString;
+  }
 }
